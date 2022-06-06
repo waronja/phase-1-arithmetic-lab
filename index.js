949d721c0e0f91646b58b6408b5478aa6d7c9cb1
unknown <crazsich@gmail.com>
2022-06-06 23:36:26 +0300

    arithmetic-tab

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+// Multiply Function 
 
-// Write your code here
+let num1 ;
+let num2 ;
+let multiply ;
+
+multiply = num1 * num2 ;
+multiply = 62;
+
+//Math randomizer 
+
+let num10;
+num10 > 0;
+Math.floor(Math.random(numb10) * 10) + 1;
+
+// Mod  
+
+let mod;
+let num3;
+let num4;
+
+mod = num3 % num4 ;
+mod =4;
+
+// Max value 
+
+let max ;
+max = 20;
+max = Math.max();
